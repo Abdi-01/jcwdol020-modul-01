387b90813a886fbd6756bb3fceef9a93a44b30af
Abdi-01 <abdialghi@gmail.com>
2025-04-29 19:32:56 +0700

    function

---
+++ Day06/function.ts
@@ -0,0 +1,23 @@
+// Function
+/**
+ * - Definisi : Sekumpulan program yang ditujukan untuk menyelesaikan suatu proses khusus.
+ *              - Untuk menggunakan function, maka harus dibuat terlebih dahulu.
+ *              - Function bersifat reusable dan modular
+ * - Kategori : Built-in function dan User-defined function
+ * - Aturan :
+ *      - Nama function mengacu pada aturan penamaan variable
+ *      - Format penulisan :
+ *          - Declarative function : 
+ *                  function namaFunc(param1?, param2?,...){
+ *                      - code
+ *                  }
+ *         - Function expression :
+ *                  const namaFunc = function(param1?, param2?,...){
+ *                      - code
+ *                  } 
+ *         - Arrow function
+ *                  a. const namaFunc = (param1?, param2?,...) => - code
+ *                  b. const namaFunc = (param1?, param2?,...) => {
+ *                          - code
+ *                     }
+ */
