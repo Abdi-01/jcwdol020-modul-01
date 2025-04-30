c3e532b7fb4499986744bfa0bdf957dad43a68c4
Abdi-01 <abdialghi@gmail.com>
2025-04-30 21:00:38 +0700

    cbfn

--- Day06/function.ts
+++ Day06/function.ts
@@ -38,6 +38,7 @@ penjumlahan; // memanggil fungsi
 // penjumlahan(); // menjalankan fungsi
 
 console.log(penjumlahan(15, 2));
+console.log(penjumlahan(10, 12));
 
 // CALLBACK FUNCTION : sebuah function yang dititipkan lewat parameter untuk dijalankan
 //                     didalam function utama pemilik parameter
@@ -56,6 +57,65 @@ function perkalian(valueA: number, valueB: number) {
 
 renderCalculator(12, 5, pengurangan);
 renderCalculator(12, 5, perkalian);
+// Callbackfn with Function expression
 renderCalculator(12, 5, function (nilaiA: number, nilaiB: number) {
     return nilaiA / nilaiB;
+});
+
+// Callbackfn with Arrow function
+renderCalculator(12, 5, (nilaiA: number, nilaiB: number) => {
+    return nilaiA / nilaiB;
+});
+
+// array.forEach(), array.map()
+
+// array.forEach(callback) : mengakses setiap value pada suatu array
+const nameList: string[] = ["Edo", "Kevin", "Marsel"];
+
+for (let i: number = 0; i < nameList.length; i++) {
+    console.log(i);
+    console.log(nameList[i]);
+};
+
+nameList.forEach(function (value: string, index: number) {
+    console.log(index);
+    console.log(value);
+});
+
+let numberList: number[] = [2, 3, 4, 5, 1];
+
+numberList.forEach((data: number, i: number) => {
+    console.log(data);
+    console.log(i);
+})
+
+// array.map(callback) : mengakses setiap value pada array dan dijadikan data array baru
+let priceList: number[] = [2000, 20000, 200000, 2500000];// --> ["Rp 2.000", "Rp 20.000", "Rp 200.000", "Rp 2.500.000"]
+let currencyList: string[] = priceList.map((value: number) => {
+    return value.toLocaleString("id-ID", {
+        style: "currency",
+        currency: "IDR",
+        minimumFractionDigits: 0
+    })
+});
+
+console.log(currencyList);
+
+// Exercise : Coba pahami cara kerja forEach, lalu duplikasi kedalam function yg kalian buat sendiri
+
+let usia: number[] = [12, 17, 20];
+usia.forEach((value: number, index: number) => {
+    console.log(value);
+});
+
+function duplicateForEach(dataArray: any[], callback: any) {
+    // what the code ?
+    for (let i = 0; i < dataArray.length; i++) {
+        callback(dataArray[i], i);
+    }
+};
+
+duplicateForEach(usia, (value: number, index: number) => {
+    console.log(value);
+    console.log(index);
 });
